b53b0baae17eca2a40fa37ca5c7a7430ff43a51f
Olga Bezdetko <olgabezdetko1994@gmail.com>
2021-05-16 00:23:23 +0300

    timer

--- timer.js
+++ timer.js
@@ -1,2 +1,39 @@
+const refs = {
+  daysNum: document.querySelector('[data-value="days"]'),
+  hoursNum: document.querySelector('[data-value="hours"]'),
+  minsNum: document.querySelector('[data-value="mins"]'),
+  secsNum: document.querySelector('[data-value="secs"]'),
+};
 
-import './sass/main.scss';
+class CountdownTimer {
+    constructor({selector, targetDate}) {
+        this.selector = document.querySelector(selector);
+        this.targetDate = targetDate;
+    };
+    startTimer() {
+        setInterval(() => {
+            const currentTime = Date.now();
+            const time = this.targetDate - currentTime;
+            const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+            const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+            const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+            const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+            this.countTime(days, hours, mins, secs);
+        }, 1000)
+    };
+    countTime(days, hours, mins, secs) {
+    this.selector.querySelector('[data-value="days"]').textContent = days;
+    this.selector.querySelector('[data-value="hours"]').textContent = hours;
+    this.selector.querySelector('[data-value="mins"]').textContent = mins;
+    this.selector.querySelector('[data-value="secs"]').textContent = secs;
+    };
+    pad(value) {
+      return String(value).padStart(2, '0');
+    };
+};
+
+const timerDate = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('May 31, 2021'),
+}).startTimer();
+
